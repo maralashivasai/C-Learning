74efe851cc5589791829626c9676812d4ff92ade
Shivasai <shivasai.marala@hcl.com>
2021-03-18 16:20:54 +0530

    fixed  error

--- DEMO4.c
+++ DEMO4.c
@@ -23,11 +23,15 @@ printf(" inside main %u\n",strlens(s));
 
 int * strlens(char  d[])
 {
-int *n=(int *)calloc(1,sizeof(int));
+int *n=(int *)malloc(sizeof(int));
  
 printf("enter n value");
   scanf("%d",n);
-//   *n=4;
+int *n1=(int *)malloc(sizeof(int));
+printf("n PREV %d\n",n);
+   n=n1;
+printf("%d\n",n);
+*n=12;
 
    printf("inside function %u\n",n);
 
@@ -37,3 +41,8 @@ return n;
 
 	
 }
+
+
+
+              
+
